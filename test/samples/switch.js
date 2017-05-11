b9e39253d15c05b4d763b025e5b029e432566b55
Rich-Harris <richard.a.harris@gmail.com>
2017-05-11 11:19:10 -0400

    failing test for #19

--- test/samples/switch.js
+++ test/samples/switch.js
@@ -1,7 +1,6 @@
 module.exports = [
 	{
 		description: 'removes whitespace in switch statement',
-
 		input: `
 			switch ( foo ) {
 				case  bar :
@@ -15,7 +14,18 @@ module.exports = [
 				default :
 					c();
 			}`,
-
 		output: `switch(foo){case bar:a();break;case baz:b();break;default:c()}`
+	},
+
+	{
+		solo: true,
+		description: 'TK',
+		input: `
+			function foo () {
+				switch (a) {
+					case 0: while (++i < l) { b(); } return;
+				}
+			};`,
+		output: `function foo(){switch(a){case 0:while(++i<l)b();return}}`
 	}
 ];
