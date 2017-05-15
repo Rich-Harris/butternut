ef9859d5acbdc3c69266e18a98eb2bedf4385275
Rich-Harris <richard.a.harris@gmail.com>
2017-05-15 16:30:30 -0400

    handle empty switch statements

--- src/program/types/SwitchStatement.js
+++ src/program/types/SwitchStatement.js
@@ -1,24 +1,43 @@
 import Node from '../Node.js';
+import { UNKNOWN } from '../../utils/sentinels.js';
 
 export default class SwitchStatement extends Node {
+	initialise ( scope ) {
+		super.initialise( scope );
+
+		if ( this.cases.length === 0 ) {
+			const value = this.discriminant.getValue();
+			this.skip = value !== UNKNOWN || this.discriminant.type === 'Identifier';
+		}
+	}
+
 	minify ( code ) {
-		if ( this.discriminant.start > this.start + 7 ) {
-			code.overwrite( this.start + 6, this.discriminant.start, '(' );
+		// special (and unlikely!) case — no cases, but a non-removable discriminant
+		if ( this.cases.length === 0 ) {
+			this.discriminant.minify( code );
+			code.remove( this.start, this.discriminant.start );
+			code.remove( this.discriminant.end, this.end );
 		}
 
-		let c = this.discriminant.end;
-
-		this.cases.forEach( ( switchCase, i ) => {
-			if ( switchCase.start > c + ( i ? 1 : 2 ) ) {
-				code.overwrite( c, switchCase.start, i ? ';' : '){' );
+		else {
+			if ( this.discriminant.start > this.start + 7 ) {
+				code.overwrite( this.start + 6, this.discriminant.start, '(' );
 			}
 
-			c = switchCase.end;
-			while ( code.original[ c - 1 ] === ';' ) c -= 1;
-		});
+			let c = this.discriminant.end;
 
-		if ( this.end > c + 1 ) code.overwrite( c, this.end, '}' );
+			this.cases.forEach( ( switchCase, i ) => {
+				if ( switchCase.start > c + ( i ? 1 : 2 ) ) {
+					code.overwrite( c, switchCase.start, i ? ';' : '){' );
+				}
 
-		super.minify( code );
+				c = switchCase.end;
+				while ( code.original[ c - 1 ] === ';' ) c -= 1;
+			});
+
+			if ( this.end > c + 1 ) code.overwrite( c, this.end, '}' );
+
+			super.minify( code );
+		}
 	}
 }
