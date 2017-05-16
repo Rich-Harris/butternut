c1c82b4bf3ee58e32d51c00e5ff28fe06aca8691
Rich-Harris <richard.a.harris@gmail.com>
2017-05-15 20:22:54 -0400

    fix handling of nested parenthesized expressions

--- src/program/types/ParenthesizedExpression.js
+++ src/program/types/ParenthesizedExpression.js
@@ -1,9 +1,6 @@
 import Node from '../Node.js';
 
 function shouldRemoveParens ( expression, parent ) {
-	const expressionPrecedence = expression.getPrecedence();
-	const parentPrecedence = parent.getPrecedence();
-
 	if ( /^Object/.test( expression.getLeftHandSide().type ) || /^Object/.test( expression.getRightHandSide().type ) ) {
 		return false;
 	}
@@ -18,6 +15,9 @@ function shouldRemoveParens ( expression, parent ) {
 			( parent.type === 'ExpressionStatement' && parent.parent.type === 'CallExpression' )
 		);
 	}
+
+	const expressionPrecedence = expression.getPrecedence();
+	const parentPrecedence = parent.getPrecedence();
 
 	if ( parentPrecedence > expressionPrecedence ) return false;
 	if ( expressionPrecedence > parentPrecedence ) return true;
@@ -46,21 +46,16 @@ export default class ParenthesizedExpression extends Node {
 	}
 
 	minify ( code ) {
+		let start = this.start;
+		let end = this.end;
 		let parent = this.parent;
 
-		// TODO we can do two remove operations — one at the start, one at the
-		// end. don't need to do it on each descent
-
 		let expression = this.expression;
-		while ( expression.type === 'ParenthesizedExpression' ) {
-			code.remove( this.start, expression.start );
-			code.remove( expression.end, this.end );
-			expression = expression.expression;
-		}
+		while ( expression.type === 'ParenthesizedExpression' ) expression = expression.expression;
 
 		if ( shouldRemoveParens( expression, parent ) ) {
-			code.remove( this.start, expression.start );
-			code.remove( expression.end, this.end );
+			code.remove( start, expression.start );
+			code.remove( expression.end, end );
 		} else {
 			if ( expression.start > this.start + 1 ) code.remove( this.start + 1, expression.start );
 			if ( this.end > expression.end + 1 ) code.remove( expression.end, this.end - 1 );
