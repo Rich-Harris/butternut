87070b50fecb125de9d28bbae80e3c96aadd8f40
Rich-Harris <richard.a.harris@gmail.com>
2017-05-16 13:26:07 -0400

    remove leading zero from numbers, and use e notation where appropriate

--- src/program/types/VariableDeclaration.js
+++ src/program/types/VariableDeclaration.js
@@ -42,12 +42,12 @@ export default class VariableDeclaration extends Node {
 			c = declarator.end;
 		}
 
-		// we may have been asked to declare some additional vars, if they were
-		// declared inside blocks that have been removed
-		if ( this.rideAlongs ) code.appendLeft( c, `,` + this.rideAlongs.join( ',' ) );
-
 		if ( this.end > c + 1 ) code.remove( c, this.end - 1 ); // TODO semi-less declarations
 
 		declarations.forEach( declarator => declarator.minify( code ) );
+
+		// we may have been asked to declare some additional vars, if they were
+		// declared inside blocks that have been removed
+		if ( this.rideAlongs ) code.appendLeft( c, `,` + this.rideAlongs.join( ',' ) );
 	}
 }
