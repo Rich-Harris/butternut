9f7ef702e88b11bdffe9996565c959b7f06be865
Rich-Harris <richard.a.harris@gmail.com>
2017-05-16 17:37:28 -0400

    remove space before folded return statement argument if possible

--- src/program/types/ReturnStatement.js
+++ src/program/types/ReturnStatement.js
@@ -1,4 +1,6 @@
 import Node from '../Node.js';
+import { UNKNOWN } from '../../utils/sentinels.js';
+import stringify from '../../utils/stringify.js';
 
 const invalidChars = /[a-zA-Z$_0-9/]/;
 
@@ -6,9 +8,11 @@ export default class ReturnStatement extends Node {
 	minify ( code ) {
 		if ( !this.argument ) return;
 
-		const expression = this.argument.getLeftHandSide();
+		const value = this.argument.getValue();
 
-		const needsTrailingWhitespace = invalidChars.test( code.original[ expression.start ] );
+		const needsTrailingWhitespace = value === UNKNOWN ?
+			invalidChars.test( code.original[ this.argument.getLeftHandSide().start ] ) :
+			invalidChars.test( stringify( value )[0] );
 
 		if ( needsTrailingWhitespace && this.argument.start === this.start + 6 ) {
 			// ensure that parenthesized expression isn't too close
